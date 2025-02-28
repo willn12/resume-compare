46a18e2a8c6f6df9c61754b48ec46054a81bb68e
wnylund <willn@vt.edu>
2025-02-28 15:01:17 -0500

    redploy

--- next-frontend/next.config.js
+++ next-frontend/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     swcMinify: true,
+    images: {
+        unoptimized: true
+    },
     async headers() {
       return [
         {
